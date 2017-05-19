c68eb1dc3b559dd0bc6bdc26b49fb6d68204f519
Muqtadir Ahmed <muqtadir007@gmail.com>
2017-05-19 11:41:28 +0530

    Initial commit

---
+++ Smartally Support/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smartally Support
+//
+//  Created by Muqtadir Ahmed on 19/05/17.
+//  Copyright © 2017 Bitjini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
